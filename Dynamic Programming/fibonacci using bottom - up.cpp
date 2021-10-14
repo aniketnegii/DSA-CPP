12cab0e54b50dd179405aa31428f336c1c458d34
Anshul <54281291+Anshul123kumar@users.noreply.github.com>
2021-10-15 00:53:24 +0530

    Add files via upload

---
+++ Dynamic Programming/fibonacci using bottom - up.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int n=5;
+	int dp[n+1];
+	
+	dp[0] = 0;
+	dp[1] = 1;
+	
+	for(int i=2; i<=n; i++){
+		dp[i] = dp[i-1]+dp[i-2]; 
+	}
+	cout<<dp[n];
+}
