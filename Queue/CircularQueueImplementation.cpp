63f7801234f5b3d836b3c35aba2c8d319a522c18
anonymous-tiger <poonamrani.pr71@gmail.com>
2021-10-19 08:29:00 +0530

    refactored code

--- Queue/CircularQueueImplementation.cpp
+++ Queue/CircularQueueImplementation.cpp
@@ -1,59 +1,128 @@
-// Program to impement queue data structue using arrays
-#include <iostream>
+// Program to impement circular queue data structue 
+#include<bits/stdc++.h>
 using namespace std;
-int queue[100], n = 100, front = - 1, rear = - 1;
-void Insert() {
-   int val;
-   if (rear == n - 1)
-   cout<<"Queue Overflow"<<endl;
-   else {
-      if (front == - 1)
-      front = 0;
-      cout<<"Insert the element in queue : "<<endl;
-      cin>>val;
-      rear++;
-      queue[rear] = val;
-   }
+
+class Queue
+{
+	// Initialize front and rear
+	int rear, front;
+
+	// Circular Queue
+	int size;
+	int *arr;
+
+	Queue(int s)
+	{
+	front = rear = -1;
+	size = s;
+	arr = new int[s];
+	}
+
+	void enQueue(int value);
+	int deQueue();
+	void displayQueue();
+};
+
+
+/* Function to create Circular queue */
+void Queue::enQueue(int value)
+{
+	if ((front == 0 && rear == size-1) ||
+			(rear == (front-1)%(size-1)))
+	{
+		printf("\nQueue is Full");
+		return;
+	}
+
+	else if (front == -1) /* Insert First Element */
+	{
+		front = rear = 0;
+		arr[rear] = value;
+	}
+
+	else if (rear == size-1 && front != 0)
+	{
+		rear = 0;
+		arr[rear] = value;
+	}
+
+	else
+	{
+		rear++;
+		arr[rear] = value;
+	}
 }
-void Delete() {
-   if (front == - 1 || front > rear) {
-      cout<<"Queue Underflow ";
-      return ;
-   } else {
-      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
-      front++;;
-   }
+
+// Function to delete element from Circular Queue
+int Queue::deQueue()
+{
+	if (front == -1)
+	{
+		printf("\nQueue is Empty");
+		return INT_MIN;
+	}
+
+	int data = arr[front];
+	arr[front] = -1;
+	if (front == rear)
+	{
+		front = -1;
+		rear = -1;
+	}
+	else if (front == size-1)
+		front = 0;
+	else
+		front++;
+
+	return data;
 }
-void Display() {
-   if (front == - 1)
-   cout<<"Queue is empty"<<endl;
-   else {
-      cout<<"Queue elements are : ";
-      for (int i = front; i <= rear; i++)
-      cout<<queue[i]<<" ";
-         cout<<endl;
-   }
+
+// Function displaying the elements
+// of Circular Queue
+void Queue::displayQueue()
+{
+	if (front == -1)
+	{
+		printf("\nQueue is Empty");
+		return;
+	}
+	printf("\nElements in Circular Queue are: ");
+	if (rear >= front)
+	{
+		for (int i = front; i <= rear; i++)
+			printf("%d ",arr[i]);
+	}
+	else
+	{
+		for (int i = front; i < size; i++)
+			printf("%d ", arr[i]);
+
+		for (int i = 0; i <= rear; i++)
+			printf("%d ", arr[i]);
+	}
 }
-int main() {
-   int ch;
-   cout<<"1) Insert element to queue"<<endl;
-   cout<<"2) Delete element from queue"<<endl;
-   cout<<"3) Display all the elements of queue"<<endl;
-   cout<<"4) Exit"<<endl;
-   do {
-      cout<<"Enter your choice : "<<endl;
-      cin<<ch;
-      switch (ch) {
-         case 1: Insert();
-         break;
-         case 2: Delete();
-         break;
-         case 3: Display();
-         break;
-         case 4: cout<<"Exit"<<endl;
-         break;
-         default: cout<<"Invalid choice"<<endl;
-      }
-   } while(ch!=4);
-   return 0;
+
+int main()
+{
+	Queue q(5);
+	q.enQueue(14);
+	q.enQueue(22);
+	q.enQueue(13);
+	q.enQueue(-6);
+
+	q.displayQueue();
+
+	printf("\nDeleted value = %d", q.deQueue());
+	printf("\nDeleted value = %d", q.deQueue());
+
+	q.displayQueue();
+
+	q.enQueue(9);
+	q.enQueue(20);
+	q.enQueue(5);
+
+	q.displayQueue();
+
+	q.enQueue(20);
+	return 0;
 }
